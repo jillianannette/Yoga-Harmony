d471e6a2dac1abfa1afd8b001d63e0acafd42462
Jill M <mora.jillian@gmail.com>
2016-07-27 23:14:07 -0500

    Content Edit
    Training testimonials.

--- assets/mobirise/js/script.js
+++ assets/mobirise/js/script.js
@@ -423,3 +423,10 @@
         document.body.insertBefore(e, document.body.childNodes[0]);
     }
 }();
+
+$(document).ready(function() {
+  //carousel options
+  $('#quote-carousel').carousel({
+    pause: true, interval: 10000,
+  });
+});
